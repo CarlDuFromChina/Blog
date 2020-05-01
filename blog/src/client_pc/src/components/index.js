13546fc3811758b7c526d5e81ae873f5e48c62d3
carldu <carldu@rektec.com.cn>
2020-05-01 12:20:35 +0800

    添加博客卡片组件

--- blog/src/client_pc/src/components/index.js
+++ blog/src/client_pc/src/components/index.js
@@ -1,7 +1,9 @@
 import spBlogTable from './spBlogTable';
+import spBlogCard from './spBlogCard';
 
 const components = [
-  { name: spBlogTable.name, component: spBlogTable }
+  { name: spBlogTable.name, component: spBlogTable },
+  { name: spBlogCard.name, component: spBlogCard }
 ];
 
 export default components;
