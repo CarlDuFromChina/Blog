ad167e773612646f0f16881c063f51ebf529df95
carldu <carldu@rektec.com.cn>
2020-05-31 13:53:35 +0800

    添加关于我

--- blog/src/client_pc/src/moudule/home/aboutme.vue
+++ blog/src/client_pc/src/moudule/home/aboutme.vue
@@ -1,7 +1,16 @@
 <template>
   <el-container>
     <el-aside width="60%">
-      <sp-section title="关于我"> </sp-section>
+      <sp-section title="关于我">
+        <el-card>
+          <div class="about">
+            <p><span>我的名字是：</span>杜淼鑫</p>
+            <p><span>职业：</span>程序员</p>
+            <p><span>爱好：</span>写代码、读科幻小说、跑步</p>
+            <p><span>座右铭：</span>既往不恋，当下不杂，未来不迎</p>
+          </div>
+        </el-card>
+      </sp-section>
     </el-aside>
     <el-aside width="30%" style="padding-left:40px;">
       <sp-section title="名片">
@@ -21,3 +30,16 @@ export default {
   }
 };
 </script>
+
+<style lang="less" scoped>
+.about {
+  text-align: center;
+  min-height: 500px;
+  > p {
+    color: #606266;
+    > span {
+      color: #303133;
+    }
+  }
+}
+</style>
