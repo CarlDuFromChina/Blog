6bf3a1391b47d0985480b74c63ad2d60d04a27b5
Kar <18556906294@163.com>
2020-07-05 10:25:41 +0800

    优化推荐信息

--- blog/src/client_pc/src/module/admin/recommendInfo/recommendInfoList.vue
+++ blog/src/client_pc/src/module/admin/recommendInfo/recommendInfoList.vue
@@ -5,47 +5,28 @@
     :operations="operations"
     :columns="columns"
     :editComponent="editComponent"
-    :allow-select="!isReadonly"
-    :header-click="openLink"
-    v-bind="$attrs"
-    v-on="$listeners"
+    allow-select
   ></sp-list>
 </template>
 
 <script>
-import RecommendInfoEdit from './RecommendInfoEdit';
+import recommendInfoEdit from './RecommendInfoEdit';
 
 export default {
   name: 'RecommendInfoList',
   data() {
     return {
       controllerName: 'RecommendInfo',
-      editComponent: RecommendInfoEdit
-    };
-  },
-  computed: {
-    operations() {
-      return !this.isReadonly ? ['new', 'delete', 'search'] : [];
-    },
-    columns() {
-      const editColumns = [
+      editComponent: recommendInfoEdit,
+      columns: [
         { prop: 'name', label: '名称', width: 300 },
         { prop: 'url', label: '地址', width: 500 },
         { prop: 'recommend_typeName', label: '类型' },
         { prop: 'createdByName', label: '创建人' },
         { prop: 'createdOn', label: '创建日期', type: 'datetime' }
-      ];
-      const readonlyColumns = [{ prop: 'name', label: '最新推荐' }];
-      return !this.isReadonly ? editColumns : readonlyColumns;
-    },
-    isReadonly() {
-      return this.$attrs.type === 'readonly';
-    }
-  },
-  methods: {
-    openLink(row) {
-      window.open(row.url);
-    }
+      ],
+      operations: ['new', 'delete', 'search']
+    };
   }
 };
 </script>
