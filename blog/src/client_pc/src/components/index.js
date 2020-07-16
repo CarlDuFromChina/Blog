258aa1d66799f850979d63597e88d01b703d2907
Kar <18556906294@163.com>
2020-07-16 09:38:05 +0800

    移除组件

--- blog/src/client_pc/src/components/index.js
+++ blog/src/client_pc/src/components/index.js
@@ -1,11 +1,9 @@
 import spBlogTable from './spBlogTable';
 import spBlogCard from './spBlogCard';
-import spMarkdownEdit from './spMarkdownEdit';
 
 const components = [
   { name: spBlogTable.name, component: spBlogTable },
-  { name: spBlogCard.name, component: spBlogCard },
-  { name: spMarkdownEdit.name, component: spMarkdownEdit }
+  { name: spBlogCard.name, component: spBlogCard }
 ];
 
 export default components;
