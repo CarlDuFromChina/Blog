5673860517f209e75e5d5432d0b7a94ed6be61e0
Carl Du <carldu@rektec.com.cn>
2020-10-02 23:48:03 +0800

    修改博客路由

--- blog/src/client_pc/src/module/index.js
+++ blog/src/client_pc/src/module/index.js
@@ -1,9 +1,8 @@
-
 import index from './index/index.js';
 
 export default [
   {
-    path: '/blogReadonly/:id',
+    path: '/blog/:id',
     name: 'blogReadonly',
     component: () => import('./admin/blog/blogReadonly')
   }
