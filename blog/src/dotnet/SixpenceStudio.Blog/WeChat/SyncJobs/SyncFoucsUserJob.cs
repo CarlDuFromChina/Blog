3c93dfcd169b74b16ccee2815da8d73bea4655af
Kar <18556906294@163.com>
2020-11-17 22:41:51 +0800

    添加微信同步job

---
+++ blog/src/dotnet/SixpenceStudio.Blog/WeChat/SyncJobs/SyncFoucsUserJob.cs
@@ -0,0 +1,79 @@
+﻿#region 类文件描述
+/*********************************************************
+Copyright @ Sixpence Studio All rights reserved. 
+Author   : Karl Du
+Created: 2020/11/17 22:36:22
+Description：同步微信关注用户Job
+********************************************************/
+#endregion
+
+using Newtonsoft.Json;
+using SixpenceStudio.BaseSite.UserInfo;
+using SixpenceStudio.Platform.Data;
+using SixpenceStudio.Platform.Job;
+using SixpenceStudio.WeChat.FocusUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixpenceStudio.Blog.WeChat.SyncJobs
+{
+    public class SyncFoucsUserJob : JobBase
+    {
+        public override string Name => "同步微信关注用户";
+
+        public override string Description => "同步微信关注用户（手动）";
+
+        public override string CronExperssion => "";
+
+        public override void Execute(IPersistBroker broker)
+        {
+            var focusUserService = new FocusUserService();
+            var focusUserList = focusUserService.GetFocusUserList();
+
+            var user = broker.Retrieve<user_info>("5B4A52AF-052E-48F0-82BB-108CC834E864");
+            if (focusUserList.count > 0)
+            {
+                var user_list = new List<OpenId>();
+                focusUserList.data.openid.ForEach(item => user_list.Add(new OpenId() { openid = item, lang = "zh_CN" }));
+                var focusUsers = focusUserService.GetFocusUsers(JsonConvert.SerializeObject(new { user_list }));
+                var start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                broker.ExecuteTransaction(() =>
+                {
+                    focusUsers.user_list.ForEach(focusUser =>
+                    {
+                        var wechat_user = new wechat_user()
+                        {
+                            Id = focusUser.openid,
+                            subscribe = Convert.ToInt32(focusUser.subscribe),
+                            nickname = focusUser.nickname,
+                            name = focusUser.nickname,
+                            sex = focusUser.sex,
+                            language = focusUser.language,
+                            city = focusUser.city,
+                            province = focusUser.province,
+                            country = focusUser.country,
+                            headimgurl = focusUser.headimgurl,
+                            subscribe_time = start.AddMilliseconds(focusUser.subscribe_time * 1000).ToLocalTime(),
+                            unionid = focusUser.unionid,
+                            remark = focusUser.remark,
+                            groupid = focusUser.groupid,
+                            subscribe_scene = focusUser.subscribe_scene,
+                            qr_scene = focusUser.qr_scene,
+                            qr_scene_str = focusUser.qr_scene_str,
+                            createdBy = user.user_infoId,
+                            createdByName = user.name,
+                            modifiedBy = user.user_infoId,
+                            modifiedByName = user.name,
+                            createdOn = DateTime.Now,
+                            modifiedOn = DateTime.Now,
+                        };
+                        broker.Save(wechat_user);
+                    });
+                });
+            }
+        }
+    }
+}
---
+++ blog/src/dotnet/SixpenceStudio.Blog/WeChat/SyncJobs/SyncNewsMaterialJob.cs
@@ -0,0 +1,74 @@
+﻿#region 类文件描述
+/*********************************************************
+Copyright @ Sixpence Studio All rights reserved. 
+Author   : Karl Du
+Created: 2020/11/17 22:37:09
+Description：同步微信图文素材Job
+********************************************************/
+#endregion
+
+using Newtonsoft.Json;
+using SixpenceStudio.BaseSite.UserInfo;
+using SixpenceStudio.Platform.Data;
+using SixpenceStudio.Platform.Job;
+using SixpenceStudio.Platform.Logging;
+using SixpenceStudio.WeChat.Material;
+using SixpenceStudio.WeChat.WeChatNews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixpenceStudio.Blog.WeChat.SyncJobs
+{
+    public class SyncNewsMaterialJob : JobBase
+    {
+        public override string Name => "同步微信图文素材";
+
+        public override string Description => "同步微信图文素材（手动）";
+
+        public override string CronExperssion => "";
+
+        public override void Execute(IPersistBroker broker)
+        {
+            var logger = LogFactory.GetLogger("wechat");
+            logger.Debug("开始同步微信公众号图文素材");
+            try
+            {
+                var result = new WeChatMaterialService().GetNewsMaterial(1, 5000);
+                var user = broker.Retrieve<user_info>("5B4A52AF-052E-48F0-82BB-108CC834E864");
+                var dataList = from item in result.item
+                               select new wechat_news()
+                               {
+                                   wechat_newsId = item.media_id,
+                                   content = JsonConvert.SerializeObject(item.content),
+                                   media_id = item.media_id,
+                                   update_time = item.update_time,
+                                   name = item.content.news_item.FirstOrDefault()?.title,
+                                   author = item.content.news_item.FirstOrDefault()?.author,
+                                   createdBy = user.user_infoId,
+                                   createdByName = user.name,
+                                   modifiedBy = user.user_infoId,
+                                   modifiedByName = user.name,
+                                   createdOn = DateTime.Now,
+                                   modifiedOn = DateTime.Now,
+                               };
+
+                if (dataList == null || dataList.Count() == 0)
+                {
+                    return;
+                }
+                dataList.ToList().ForEach(item =>
+                {
+                    broker.Save(item);
+                });
+                logger.Debug("同步微信公众号图文素材成功");
+            }
+            catch (Exception e)
+            {
+                logger.Error("同步微信公众号图文素材失败", e);
+            }
+        }
+    }
+}
