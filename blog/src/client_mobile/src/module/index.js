b940c6cd85cc0f5cfc822ab9916f2df7672f528d
Kar <18556906294@163.com>
2021-01-17 21:43:37 +0800

    添加移动端

---
+++ blog/src/client_mobile/src/module/index.js
@@ -0,0 +1,5 @@
+export default [{
+  name: 'index',
+  path: '/index',
+  component: () => import('./index.vue')
+}]
---
+++ blog/src/client_mobile/src/module/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <mt-button @click.native="handleClick">Button</mt-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  methods: {
+    handleClick () {
+      this.$message.alert('Hello World!', 'Hi~')
+    }
+  }
+}
+</script>
