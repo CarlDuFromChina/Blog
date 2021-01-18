481b46513e514aeb92cd8944dc4831af88d8f2a3
Carl Du <carldu@rektec.com.cn>
2021-01-18 09:12:14 +0800

    添加移动端build

--- build.ps1
+++ build.ps1
@@ -23,6 +23,7 @@ New-Item -ItemType directory "blog\build\bin"
 $release = "blog\src\server_dotnet\build\*"
 Copy-Item -Force -Recurse $release "blog\build\bin\"
 Write-Success-Message "OK."
+Remove-Item -Recurse -Force ".\DotNetBuild.ps1"
 '@ > DotNetBuild.ps1
 
 start powershell .\DotNetBuild.ps1
@@ -41,11 +42,30 @@ Write-Success-Message "OK."
 
 $release2 = "blog\src\client_pc\dist\*"
 Copy-Item -Force -Recurse $release2 "blog\build\"
+Remove-Item -Recurse -Force ".\ClientPCBuild.ps1"
 '@ > ClientPCBuild.ps1
 
 start powershell .\ClientPCBuild.ps1 -wait
 
+# Build mobile.vue
+
+@'
+. ".\help.ps1"
+Write-Section-Message "Build pc.vue."
+Push-Location "blog\src\client_mobile"
+$npmInstallException = yarn.cmd install
+If (!$?) { Throw $npmInstallException }
+$buildException = yarn build
+If (!$?) { Throw $buildException }
+Pop-Location
+Write-Success-Message "OK."
+
+$release2 = "blog\src\client_mobile\dist\*"
+Copy-Item -Force -Recurse $release2 "blog\build\debug\"
+Remove-Item -Recurse -Force ".\ClientMobileBuild.ps1"
+'@ > ClientMobileBuild.ps1
+
+start powershell .\ClientMobileBuild.ps1 -wait
+
 # Done
-Remove-Item -Recurse -Force ".\DotNetBuild.ps1"
-Remove-Item -Recurse -Force ".\ClientPCBuild.ps1"
 Write-Section-Message "Finished!"
