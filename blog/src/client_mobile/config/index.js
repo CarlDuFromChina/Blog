1e19497662ff6fe643b7be7d7886022d9d9b542d
Carl Du <carldu@rektec.com.cn>
2021-01-18 14:38:17 +0800

    添加生产环境切换

--- blog/src/client_mobile/config/index.js
+++ blog/src/client_mobile/config/index.js
@@ -1,15 +1,15 @@
-'use strict'
+'use strict';
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: isProduction ? './' : '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: isProduction ? './' : '/',
 
     /**
      * Source Maps
@@ -73,4 +73,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-}
+};
