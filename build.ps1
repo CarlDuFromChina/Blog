ed58a9d002f24526326ff4db747e14a6b01b42f5
Carl Du <carldu@rektec.com.cn>
2021-02-10 22:56:40 +0800

    取消异步编译

--- build.ps1
+++ build.ps1
@@ -13,8 +13,6 @@ else
 New-Item -ItemType directory "blog\build\debug\"
 
 # Restore Dotnet Packages
-@'
-. ".\help.ps1"
 Write-Section-Message "Build dotnet"
 nuget restore "blog\src\server_dotnet\SixpenceStudio.Blog\SixpenceStudio.Blog.sln"
 
@@ -25,14 +23,8 @@ New-Item -ItemType directory "blog\build\bin"
 $release = "blog\src\server_dotnet\build\*"
 Copy-Item -Force -Recurse $release "blog\build\bin\"
 Write-Success-Message "OK."
-Remove-Item -Recurse -Force ".\DotNetBuild.ps1"
-'@ > DotNetBuild.ps1
-
-start powershell .\DotNetBuild.ps1
 
 # Build pc.vue
-@'
-. ".\help.ps1"
 Write-Section-Message "Build pc.vue."
 Push-Location "blog\src\client_pc"
 $npmInstallException = yarn.cmd install
@@ -44,15 +36,9 @@ Write-Success-Message "OK."
 
 $release2 = "blog\src\client_pc\dist\*"
 Copy-Item -Force -Recurse $release2 "blog\build\"
-Remove-Item -Recurse -Force ".\ClientPCBuild.ps1"
-'@ > ClientPCBuild.ps1
-
-start powershell .\ClientPCBuild.ps1 -wait
 
 # Build mobile.vue
 
-@'
-. ".\help.ps1"
 Write-Section-Message "Build mobile.vue."
 Push-Location "blog\src\client_mobile"
 $npmInstallException = yarn.cmd install
@@ -64,10 +50,6 @@ Write-Success-Message "OK."
 
 $release3 = "blog\src\client_mobile\dist\*"
 Copy-Item -Force -Recurse $release3 "blog\build\debug\"
-Remove-Item -Recurse -Force ".\ClientMobileBuild.ps1"
-'@ > ClientMobileBuild.ps1
-
-start powershell .\ClientMobileBuild.ps1 -wait
 
 # Done
 Write-Section-Message "Finished!"
