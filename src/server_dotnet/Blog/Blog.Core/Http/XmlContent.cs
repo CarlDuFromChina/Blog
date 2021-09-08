e745c3be5b50cb989e6df9c733c5152ed28329df
CarlDuFromChina <18556906294@163.com>
2021-09-08 11:16:20 +0800

    扩展HttpClient

---
+++ src/server_dotnet/Blog/Blog.Core/Http/XmlContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Blog.Core.Http
+{
+	public class XmlContent : ByteArrayContent
+	{
+		public XmlContent(string value)
+			: base(Encoding.UTF8.GetBytes(value))
+		{
+		}
+	}
+}
