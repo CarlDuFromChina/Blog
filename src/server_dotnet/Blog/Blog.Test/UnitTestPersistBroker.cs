eeee4bd6e1e4493bbe89ea152f68ef358cf2df87
Karl Du <18556906294@163.com>
2021-09-11 18:17:02 +0800

    添加单元测试

---
+++ src/server_dotnet/Blog/Blog.Test/UnitTestPersistBroker.cs
@@ -0,0 +1,22 @@
+﻿using Blog.Core;
+using Blog.Core.Data;
+using Blog.Core.Utils;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blog.Test
+{
+    [TestFixture]
+    public class UnitTestPersistBroker
+    {
+        [Test]
+        public void TestPersistBroker()
+        {
+            var broker = PersistBrokerFactory.GetPersistBroker("Host=127.0.0.1;Port=5432;User ID=postgres;Password=123123;Database=postgres;");
+            Assert.IsNotNull(broker.DbClient);
+        }
+    }
+}
