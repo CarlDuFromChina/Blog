fb7324f7299aaa0c959a7ca6a03b9716a416c447
Karl Du <18556906294@163.com>
2021-10-11 18:42:11 +0800

    菜单权限更新

---
+++ src/server_dotnet/Blog/Blog.Core/Module/SysMenu/SysMenuPlugin.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Core.Auth.Privilege;
+using Sixpence.EntityFramework.Broker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blog.Core.Module.SysMenu
+{
+    public class SysMenuPlugin : IPersistBrokerPlugin
+    {
+        public void Execute(PersistBrokerPluginContext context)
+        {
+            var broker = context.Broker;
+            switch (context.Action)
+            {
+                case EntityAction.PostCreate:
+                    // 重新注册权限并清除缓存
+                    UserPrivilegesCache.Clear(broker);
+                    break;
+                case EntityAction.PostDelete:
+                    var privileges = new SysRolePrivilegeService(broker).GetPrivileges(context.Entity.Id).ToArray();
+                    broker.Delete(privileges);
+                    UserPrivilegesCache.Clear(broker);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
