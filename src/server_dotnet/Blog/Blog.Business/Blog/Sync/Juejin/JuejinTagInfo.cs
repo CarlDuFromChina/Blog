df2538c85b236e5d2989aae4a9b6f2e7eb231cc1
Karl Du <18556906294@163.com>
2022-05-12 23:53:55 +0800

    juejin sync add tags

---
+++ src/server_dotnet/Blog/Blog.Business/Blog/Sync/Juejin/JuejinTagInfo.cs
@@ -0,0 +1,36 @@
+﻿namespace Blog.Business.Blog.Sync.Juejin
+{
+    public class JuejinTagInfo
+    {
+        public string tag_id { get;set; }
+        public JuejinTag tag { get;set; }
+        public JuejinUserInteract user_interact { get;set; }
+    }
+
+    public class JuejinTag
+    {
+        public int id { get; set; }
+        public string tag_id { get; set; }
+        public string tag_name { get; set; }
+        public string color { get; set; }
+        public string icon { get; set; }
+        public string back_ground { get; set; }
+        public int show_navi { get; set; }
+        public int ctime { get; set; }
+        public int mtime { get; set; }
+        public int id_type { get; set; }
+        public string tag_alias { get; set; }
+        public int post_article_count { get; set; }
+        public int concern_user_count { get; set; }
+    }
+
+    public class JuejinUserInteract
+    {
+        public int id { get; set; }
+        public int omitempty { get; set; }
+        public int user_id { get; set; }
+        public string is_digg { get; set; }
+        public string is_follow { get; set; }
+        public string is_collect { get; set; }
+    }
+}
--- src/server_dotnet/Blog/Blog.Business/Blog/Sync/Juejin/SyncBlog2Juejin.cs
+++ src/server_dotnet/Blog/Blog.Business/Blog/Sync/Juejin/SyncBlog2Juejin.cs
@@ -1,9 +1,11 @@
 ﻿using Blog.Core.Module.SysConfig;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Sixpence.Common;
 using Sixpence.Common.Utils;
 using Sixpence.ORM.EntityManager;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Blog.Business.Blog.Sync.Juejin
 {
@@ -18,20 +20,80 @@ namespace Blog.Business.Blog.Sync.Juejin
 
             var data = Manager.QueryFirst<blog>(id);
             var cookie = new SysConfigService().GetValue("juejin_cookie")?.ToString();
+
             AssertUtil.CheckIsNullOrEmpty<SpException>(cookie, "博客同步到掘金需要先设置Cookie信息", "52669331-ED29-4E9B-8D2C-B5F671852BC1");
-            CreateDraft(data, cookie);
+
+            var draft = CreateDraft(data, cookie);
+            PublishDarft(draft.id, cookie);
         }
 
-        private void CreateDraft(blog blog, string cookie)
+        /// <summary>
+        /// 创建草稿
+        /// </summary>
+        /// <param name="blog"></param>
+        /// <param name="cookie"></param>
+        /// <returns></returns>
+        private JuejinDraftCreateResponseData CreateDraft(blog blog, string cookie)
         {
-            var draft = new Draft() { title = blog.title, mark_content = blog.content };
-            var postData = new Dictionary<string, string>();
-            postData.Add("cookie", cookie);
+            var draft = new Draft()
+            {
+                title = blog.title,
+                mark_content = blog.content,
+                brief_content = blog.brief,
+                tag_ids = new List<string>()
+            };
 
-            var response = HttpUtil.Post("https://api.juejin.cn/content_api/v1/article_draft/create?aid=2608&uuid=6980253040042001932", JsonConvert.SerializeObject(draft), postData);
+            var blogTags = blog.tags?.ToObject<List<string>>();
+            if (!blogTags.IsEmpty())
+            {
+                QueryTags().ForEach(item =>
+                {
+                    var tag = blogTags.Find(e => e.Equals(item.tag.tag_name, System.StringComparison.OrdinalIgnoreCase));
+                    if (!string.IsNullOrEmpty(tag))
+                    {
+                        draft.tag_ids.Add(item.tag_id);
+                    }
+                });
+            }
+
+            var headers = new Dictionary<string, string>() { { "cookie", cookie } };
+            var response = HttpUtil.Post("https://api.juejin.cn/content_api/v1/article_draft/create?aid=2608&uuid=6980253040042001932", JsonConvert.SerializeObject(draft), headers);
             var resp = JsonConvert.DeserializeObject<JuejinResponse<JuejinDraftCreateResponseData>>(response);
 
             AssertUtil.CheckBoolean<SpException>(resp.err_no != 0, $"草稿创建失败，错误信息：{resp.err_msg}", "52669331-ED29-4E9B-8D2C-B5F671852BC1");
+
+            return resp.data;
+        }
+
+        /// <summary>
+        /// 发布草稿
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cookie"></param>
+        private void PublishDarft(string id, string cookie)
+        {
+            var param = new
+            {
+                draft_id = "",
+                sync_to_org = false,
+                column_ids = new List<string>()
+            };
+            var headers = new Dictionary<string, string>() { { "cookie", cookie } };
+            var response = HttpUtil.Post("/content_api/v1/article/publish?aid=2608&uuid=6897189016987272712", JsonConvert.SerializeObject(param), headers);
+            var resp = JsonConvert.DeserializeObject<JuejinResponse<JuejinDraftPublish>>(response);
+
+            AssertUtil.CheckBoolean<SpException>(resp.err_no != 0, $"博客发布失败，错误信息：{resp.err_msg}", "52669331-ED29-4E9B-8D2C-B5F671852BC1");
+        }
+
+        /// <summary>
+        /// 查询标签
+        /// </summary>
+        /// <returns></returns>
+        private List<JuejinTagInfo> QueryTags()
+        {
+            var response = HttpUtil.Get("https://api.juejin.cn/tag_api/v1/query_tag_list?aid=2608&uuid=6897189016987272712");
+            var resp = JsonConvert.DeserializeObject<JuejinResponse<List<JuejinTagInfo>>>(response);
+            return resp.data;
         }
     }
 }
